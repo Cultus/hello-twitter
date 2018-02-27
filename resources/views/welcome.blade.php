a482358f07962a0bd9c4a7ceefcdec561df4e797
Cultus <cultus@example.com>
2018-02-27 05:00:29 +0000

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,121 +7,30 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-            </div>
-        </div>
-    </body><!--Footer-->
-    <footer class="page-footer font-small blue pt-4 mt-4">
-    
-        <!--Footer Links-->
-        <div class="container-fluid text-center text-md-left">
+        <div class="container">
+          <div class="page-header" id="banner">
             <div class="row">
-    
-                <!--First column-->
-                <div class="col-md-6">
-                    <h5 class="text-uppercase">Footer Content</h5>
-                    <p>Here you can use rows and columns here to organize your footer content.</p>
-                </div>
-                <!--/.First column-->
-    
-                <!--Second column-->
-                <div class="col-md-6">
-                    <h5 class="text-uppercase">Links</h5>
-                    <ul class="list-unstyled">
-                        <li><a href="#!">Link 1</a></li>
-                        <li><a href="#!">Link 2</a></li>
-                        <li><a href="#!">Link 3</a></li>
-                        <li><a href="#!">Link 4</a></li>
-                    </ul>
-                </div>
-                <!--/.Second column-->
+              <div class="col-lg-8 col-md-7 col-sm-6">
+                <h1>Superhero</h1>
+                <p class="lead">The brave and the blue</p>
+              </div>
             </div>
+          </div>
+          <div class="flex-center position-ref full-height">
+              @component('jumbotron')
+              @endcomponent
+          </div>
         </div>
-        <!--/.Footer Links-->
-    
-        <!--Copyright-->
-        <div class="footer-copyright py-3 text-center">
-            <div class="container-fluid">
-                © 2015 Copyright: <a href="https://mdbootstrap.com/material-design-for-bootstrap/"> MDBootstrap.com </a>
-    
-            </div>
+        <div class="content">
         </div>
-        <!--/.Copyright-->
-    
-    </footer>
-    <!--/.Footer-->
+
+        <!-- Javascript -->
+        <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    </body>
 </html>

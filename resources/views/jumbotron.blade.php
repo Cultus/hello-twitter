50665af34462aa7b7c77f44df3b74e0f4cc061bf
Cultus <hiroshi.makita.bc4@gmail.com>
2018-03-08 23:34:33 +0900

    update

--- resources/views/jumbotron.blade.php
+++ resources/views/jumbotron.blade.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
     <h2>Make an order to remintter</h2>
     <p>Create a <span class="text-primary">schedule</span> to Remintter.</p>
-    <p><a href={{ route('twitter') }}>Let's try Remitter!</p>
+    <p><a href={{ route('redirect') }}>Let's try Remitter!</p>
 </div>
